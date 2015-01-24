9d02adb107bc8dce848d28654e517b3e51dc2cb9
Michael Powell <powellm123@gmail.com>
2015-01-24 10:02:10 -0700

    First Commit

---
+++ New Unity Project/Assets/JSPyhsic.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var attractedTo : GameObject;
+var strengthOfAttraction : float = 5.0f;
+private var speed : Vector2;
+
+function Start ()
+{
+	speed = new Vector2(1, -105);
+}
+
+function Update ()
+{
+	var direction = attractedTo.transform.position - transform.position;
+	rigidbody2D.AddForce(speed);
+	rigidbody2D.AddForce(strengthOfAttraction * direction);
+}
